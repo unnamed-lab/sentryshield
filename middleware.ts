21c58d06ee690f91efa6f76ada8db72dba2d9297
Adebayo Anuoluwa Success <adebayo.anuoluwa02@gmail.com>
2025-03-29 10:13:31 +0100

    add configuration for components and middleware setup

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import { authMiddleware } from "@civic/auth-web3/nextjs/middleware";
+
+export default authMiddleware();
+
+export const config = {
+  // include the paths you wish to secure here
+  matcher: [
+    "/api/:path*",
+    "/admin/:path*",
+    "/((?!_next|favicon.ico|sitemap.xml|robots.txt|.*.jpg|.*.png|.*.svg|.*.gif).*)",
+  ],
+};
