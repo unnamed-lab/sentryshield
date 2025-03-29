5016c75426ab0262ef4eedea98bddf6b18b1f6ea
Adebayo Anuoluwa Success <adebayo.anuoluwa02@gmail.com>
2025-03-29 21:54:57 +0100

    chore: update ESLint configuration and add sidebar styles, update package dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,11 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "@tanstack/eslint-plugin-query"
+  ),
 ];
 
 export default eslintConfig;
